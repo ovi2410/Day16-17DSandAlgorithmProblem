ee04353d8391b4ceb6b5c9e21f2ac0a1e1f05a67
ovi2410 <guneshwarifegade@gmail.com>
2022-07-12 22:30:55 +0530

    [ovi]DS and algorithm program

---
+++ DataStructure/LinkList/OrderedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    class OrderedList
+    {
+        public void Ordered()
+        {
+            LinkList list = new LinkList();
+            string text = File.ReadAllText(@"C:\Users\DEEPAK\OneDrive\Desktop\rfp-161\PRATICE_PROBLEM\16-17DS\ConsoleApp1\LinkList\inputNumber.txt", Encoding.UTF8);
+            string[] stringSeparator = new string[] { " " };
+            string[] textArray;
+            textArray = text.Split(stringSeparator, StringSplitOptions.RemoveEmptyEntries);
+            int[] textInteger = new int[textArray.Length];
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                textInteger[i] = Convert.ToInt32(textArray[i]);
+            }
+            for (int i = 0; i < textInteger.Length; i++)
+            {
+                Console.Write(textInteger[i] + " ");
+            }
+            for (int i = 0; i < textInteger.Length; i++)
+            {
+                list.InsertSorted(textInteger[i]);
+            }
+            list.DisplayIntList();
+            Console.WriteLine("Enter a string which you want to be found:");
+            int searchInt = Convert.ToInt32(Console.ReadLine());
+            if (list.SearchInt(searchInt))
+            {
+                list.DeleteIntKeyElement(searchInt);
+            }
+            else
+            {
+                list.InsertSorted(searchInt);
+            }
+            list.DisplayIntList();
+            string intString = list.ReturnNumberString();
+            File.WriteAllText(@"C:\Users\Admin\source\repos\Saurabh\DataStructure\LinkedList\inputNumber.txt", intString);
+        }
+    }
+}
---
+++ DataStructure/Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Queue
+{
+    using System;
+
+    /// <summary>
+    /// this is main program class for Queue operations
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. CashCounter \n2.Calender");
+            Console.WriteLine("enter your option:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CashCounter checkOne = new CashCounter();
+                    checkOne.Queue();
+                    break;
+
+                case 2:
+                    Calender checkTwo = new Calender();
+                    checkTwo.GetCalender();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+            Console.ReadKey();
+            //CalenderQueue check = new CalenderQueue();
+            //check.GetCalender();
+        }
+    }
+}
+
